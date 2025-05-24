e09d1192ae9d50eeb3a8caac15ef6923eca58c30
dawillygene <eliamariki2000@gmail.com>
2025-05-24 15:14:42 +0300

    vue day learning basics

--- Vue/script.js
+++ Vue/script.js
@@ -2,47 +2,30 @@ const app = Vue.createApp({
 
     data() {
         return {
-
+        listShow: true,
             books: [
-                {
-                    title: "History of America",
-                    year: 1993
-                }, {
-
-                    title: "European Nations",
-                    year: 5644
-                },
-                {
-
-                    title: "James Brond",
-                    year: 2311
-                },
-                {
-
-                    title: "GROW RICH THE MAN OF BABILON",
-                    year: 3452
-                },
+                { name: "The Final Revival of Opal & Nev", author: "Dawnie Walton", img: "https://media.istockphoto.com/id/1771936111/photo/vintage-books-plant-in-pot-candle-in-candlestick-minimalistic-interior-decor-scandinavian.jpg?s=1024x1024&w=is&k=20&c=RbYrvMeTqTe8DU4HWr38cBlYkoeGtKUJDbhD6xrd12c=", isFav: true },
+                { name: "The Midnight Library", author: "Matt Haig", img: "https://media.istockphoto.com/id/1771936111/photo/vintage-books-plant-in-pot-candle-in-candlestick-minimalistic-interior-decor-scandinavian.jpg?s=1024x1024&w=is&k=20&c=RbYrvMeTqTe8DU4HWr38cBlYkoeGtKUJDbhD6xrd12c=", isFav: false },
+                { name: "The Invisible Life of Addie LaRue", author: "V.E. Schwab", img: "https://media.istockphoto.com/id/1771936111/photo/vintage-books-plant-in-pot-candle-in-candlestick-minimalistic-interior-decor-scandinavian.jpg?s=1024x1024&w=is&k=20&c=RbYrvMeTqTe8DU4HWr38cBlYkoeGtKUJDbhD6xrd12c=", isFav: true },
+                { name: "The Vanishing Half", author: "Brit Bennett", img: "https://media.istockphoto.com/id/1771936111/photo/vintage-books-plant-in-pot-candle-in-candlestick-minimalistic-interior-decor-scandinavian.jpg?s=1024x1024&w=is&k=20&c=RbYrvMeTqTe8DU4HWr38cBlYkoeGtKUJDbhD6xrd12c=", isFav: false },
+                { name: "Where the Crawdads Sing", author: "Delia Owens", img: "https://media.istockphoto.com/id/1771936111/photo/vintage-books-plant-in-pot-candle-in-candlestick-minimalistic-interior-decor-scandinavian.jpg?s=1024x1024&w=is&k=20&c=RbYrvMeTqTe8DU4HWr38cBlYkoeGtKUJDbhD6xrd12c=", isFav: true },
+                { name: "The Seven Husbands of Evelyn Hugo", author: "Taylor Jenkins Reid", img: "https://media.istockphoto.com/id/1771936111/photo/vintage-books-plant-in-pot-candle-in-candlestick-minimalistic-interior-decor-scandinavian.jpg?s=1024x1024&w=is&k=20&c=RbYrvMeTqTe8DU4HWr38cBlYkoeGtKUJDbhD6xrd12c=", isFav: false },
             ]
-            ,
-            visibility: true,
-            title: "The trial of brother jewel",
-            name: "dawilly brodah gene",
-            age: 75
-
         }
     },
-    methods: {
-        changeHeader() {
-            this.title = "HeavenLight Mariki"
-        }
-        ,
-        toggle() {
-            this.visibility = !this.visibility
-
+    methods:{
+        togglefav(book){
+            book.isFav = !book.isFav
         }
     }
+,
+computed:{
+    filteredBooks(){
+        return this.books.filter((book)=> book.isFav);
+    }
+}
 
-})
 
-app.mount('#app')
+});
 
+app.mount("#app");
